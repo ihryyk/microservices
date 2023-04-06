3b19afc4f4069f392bd1020e916e1a31a20f05c5
ihor_berezovskyi <ihor_berezovskyi@epam.com>
2023-04-06 20:38:15 +0300

    Register Organization-Service as Eureka Client

--- employee-service/src/main/java/com/employeeService/service/OrganizationAPIClient.java
+++ employee-service/src/main/java/com/employeeService/service/OrganizationAPIClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@FeignClient(url = "http://localhost:8084",name = "ORGANIZATION-SERVICE")
+@FeignClient(name = "ORGANIZATION-SERVICE")
 public interface OrganizationAPIClient {
 
     @GetMapping("api/organizations/{code}")
